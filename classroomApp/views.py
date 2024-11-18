28ff8107cdc0a165659fbff70678b209847d5c32
Harshit Sharma <123459218+HsAhRaSrHmIaT@users.noreply.github.com>
2024-11-18 17:09:53 +0530

    Add login credentials

--- classroomApp/views.py
+++ classroomApp/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render, redirect # type: ignore
-from .models import Room
+from django.contrib import messages # type: ignore
+from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from django.contrib.auth import authenticate, login, logout # type: ignore
 
 # rooms = [
 #     {'id': 1, 'name': 'Room 1'},
 #     {'id': 2, 'name': 'Room 2'},
 #     {'id': 3, 'name': 'Room 3'},
 # ]
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login successful")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid credentials")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+            Q(topic__name__icontains=q) |
+            Q(name__icontains=q) |
+            Q(description__icontains=q)
+        )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
     context = {
-        'rooms': rooms
+        'rooms': rooms,
+        'topics': topics,
+        'room_count': room_count,
     }
     return render(request, "base/home.html", context)
 
@@ -47,4 +75,5 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect("home")
     return render(request, "base/delete.html", context={'room': room})
+
 
